52ddf8ae16ef274a466a2914896d068676378c0e
jhonastba94 <jhonastba.v2@gmail.com>
2023-01-04 18:24:50 -0500

    first commit

---
+++ Angular-s8/src/app/login/login.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from './title/title.component';
+import { ContentComponent } from './content/content.component';
+import { ButtonComponent } from './button/button.component';
+import { CorrectoComponent } from './correcto/correcto.component';
+
+
+
+@NgModule({
+  declarations: [
+    TitleComponent,
+    ContentComponent,
+    ButtonComponent, 
+    CorrectoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TitleComponent,
+    ContentComponent,
+    ButtonComponent,
+    CorrectoComponent
+  ]
+})
+export class LoginModule { }
